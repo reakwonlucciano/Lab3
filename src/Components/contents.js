627465fadce4b440b3aea79eb9fde5df445be648
REAKWON LUCCIANO - STUDENT <G00382913@gmit.ie>
2021-10-06 11:51:22 +0100

    lab complete

---
+++ src/Components/contents.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Content extends Component{
+    render()
+    {
+        return  (
+            <div>
+                <h1>Hello world !</h1>
+                <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            </div>
+
+        );
+    }
+}
+export default Content;
